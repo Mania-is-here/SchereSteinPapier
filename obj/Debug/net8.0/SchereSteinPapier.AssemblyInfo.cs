482c3ab5197e30febd6258562a58960be69bf0f5
Detering <detering.mania@training.lug-ag.de>
2024-11-28 10:31:32 +0100

    second

--- obj/Debug/net8.0/SchereSteinPapier.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchereSteinPapier.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchereSteinPapier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d564f004b4ccaf7c4112fd4df573e384b52ad1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchereSteinPapier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchereSteinPapier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
